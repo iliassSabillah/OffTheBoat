0c1f3f99005bddf6112e05f6336502efa8cda76f
Daniel <danbanwo@gmail.com>
2017-01-21 17:35:48 -0500

    created housing, job and training reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,16 @@
 import {combineReducers} from 'redux';
 import checklistReducer from './checkList';
+import jobReducer from './jobReducer';
+import housingReducer from './housingReducer';
 
 
 import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
 	checklistReducer,
+	housingReducer,
+	trainingReducer,
+	jobReducer,
 	routing: routerReducer
 });
 
