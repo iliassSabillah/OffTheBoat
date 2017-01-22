5c0ccf07412876831a2799b0426b84da7b2b54ec
QuanAVuong <quan.a.vuong@gmail.com>
2017-01-21 22:27:50 -0500

    Maps.jsx hardcoded

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -13,56 +13,61 @@ const Maps = React.createClass({
 	},
 	componentDidMount() {
 
-		$('select').change(function() {
-		  let mode = $(this).val();
-		  resetValue();
-		  switch(mode) {
-		    // case "place":
-		    //   setplaceinput()
-		    //   break
-		    // case "directions":
-		    //   setdirectinput();
-		    //   break
-		    case "search":
-		    	setsearchinput();
-		    	break;
-		    // case "view":
-		    // 	setviewinput();
-		    // 	break
+		// $('select').change(function() {
+		//   let mode = $(this).val();
+		//   resetValue();
+		//   switch(mode) {
+		//     // case "place":
+		//     //   setplaceinput()
+		//     //   break
+		//     // case "directions":
+		//     //   setdirectinput();
+		//     //   break
+		//     case "search":
+		//     	setsearchinput();
+		//     	break;
+		//     // case "view":
+		//     // 	setviewinput();
+		//     // 	break
 
-		  }
-		});
+		//   }
+		// });
 
 		function genSearchURL(arr) {
 			return '&q='+arr[0].value.split(' ').join("+");
 		}
 
-		$('form').submit(function(event) {
-		  event.preventDefault();
-		  let params = $(this).serializeArray();
-		  console.log(params);
-		  let url = 'https://www.google.com/maps/embed/v1/';
-		  let mode = $('.mode').val();
-		  console.log(mode);
-		  let key = 'AIzaSyDbJUsdZAQ7Zlsa3L9Vdu1pM4E__7kZZDc';
-		  let param;
-		  switch(mode) {
-		    case "place":
-		      param = genPlaceURL(params);
-		      break;
-		    case "directions":
-		      param = genDirectURL(params);
-		      break;
-		     case "search":
-		      param = genSearchURL(params);
-		      break;
-		    case "view":
-		      param = genViewURL(params);
-		      break;
+		// $('form').submit(function(event) {
+		//   event.preventDefault();
+		//   let params = $(this).serializeArray();
+		//   console.log(params);
+		//   let url = 'https://www.google.com/maps/embed/v1/';
+		//   let mode = $('.mode').val();
+		//   console.log(mode);
+		//   let key = 'AIzaSyDbJUsdZAQ7Zlsa3L9Vdu1pM4E__7kZZDc';
+		//   let param;
+		//   switch(mode) {
+		//     case "place":
+		//       param = genPlaceURL(params);
+		//       break;
+		//     case "directions":
+		//       param = genDirectURL(params);
+		//       break;
+		//      case "search":
+		//       param = genSearchURL(params);
+		//       break;
+		//     case "view":
+		//       param = genViewURL(params);
+		//       break;
 
-		  }
-		  console.log(param);
-		  $('iframe').attr('src', url + mode +'?key='+ key +param);
+		//   }
+		//   console.log(param);
+		
+		  // $('iframe').attr('src', url + mode +'?key='+ key +param);
+		  $('iframe').attr('src', 
+		  	"https://www.google.com/maps/embed/v1/search?key=AIzaSyDbJUsdZAQ7Zlsa3L9Vdu1pM4E__7kZZDc&q=C4Q"
+		  	// url + mode +'?key='+ key +param
+		  	);
 		});
 
 	},
